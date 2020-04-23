09900f4ac0943d90ec48d2103a6b5aadd70f8281
Alexander Lyashuk <alexander.lyashuk@xperience.ai>
2020-04-23 12:44:13 +0300

    Multiprocessing data generation for speed up

--- scripts/dataset-generation.py
+++ scripts/dataset-generation.py
@@ -1,22 +1,19 @@
+import argparse
 import os
 import os.path as osp
-from glob import glob
 
-from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import wfdb
 from sklearn.preprocessing import scale
 from wfdb import rdrecord
-import multiprocessing as mp
 
 # Choose from peak to peak or centered
 mode = [20, 20]
 # mode = 128
 
 image_size = 128
-input_dir = '../mit-bih/*.atr'
 output_dir = '../data'
 
 # dpi fix
@@ -44,39 +41,44 @@ def plot(signal, filename):
 
 
 if __name__ == '__main__':
-    ecg_data = sorted([osp.splitext(i)[0] for i in glob(input_dir)])
-    for idx, ecg in enumerate(ecg_data):
-        name = osp.basename(ecg)
-        record = rdrecord(ecg)
-        ann = wfdb.rdann(ecg, extension='atr')
-        for sig_name, signal in zip(record.sig_name, record.p_signal.T):
-            if not np.all(np.isfinite(signal)):
-                continue
-            signal = scale(signal)
-            for i, (label, peak) in enumerate(zip(ann.symbol, ann.sample)):
-                if label == '/': label = "\\"
-                print('\r{}/{} {} [{}/{}]'.format(idx + 1, len(ecg_data) + 1, sig_name, i + 1, len(ann.symbol)), end="")
-                if isinstance(mode, list):
-                    if np.all([i > 0, i + 1 < len(ann.sample)]):
-                        left = ann.sample[i - 1] + mode[0]
-                        right = ann.sample[i + 1] - mode[1]
-                    else:
-                        continue
-                elif isinstance(mode, int):
-                    left, right = peak - mode // 2, peak + mode // 2
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', required=True)
+    args = parser.parse_args()
+
+
+    ecg = args.file
+    name = osp.basename(ecg)
+    record = rdrecord(ecg)
+    ann = wfdb.rdann(ecg, extension='atr')
+    for sig_name, signal in zip(record.sig_name, record.p_signal.T):
+        if not np.all(np.isfinite(signal)):
+            continue
+        signal = scale(signal)
+        for i, (label, peak) in enumerate(zip(ann.symbol, ann.sample)):
+            if label == '/': label = "\\"
+            print('\r{} [{}/{}]'.format(sig_name, i + 1, len(ann.symbol)), end="")
+            if isinstance(mode, list):
+                if np.all([i > 0, i + 1 < len(ann.sample)]):
+                    left = ann.sample[i - 1] + mode[0]
+                    right = ann.sample[i + 1] - mode[1]
                 else:
-                    raise Exception("Wrong mode in script beginning")
+                    continue
+            elif isinstance(mode, int):
+                left, right = peak - mode // 2, peak + mode // 2
+            else:
+                raise Exception("Wrong mode in script beginning")
 
-                if np.all([left > 0, right < len(signal)]):
-                    one_dim_data_dir = osp.join(output_dir, '1D', name, sig_name, label)
-                    two_dim_data_dir = osp.join(output_dir, '2D', name, sig_name, label)
-                    os.makedirs(one_dim_data_dir, exist_ok=True)
-                    os.makedirs(two_dim_data_dir, exist_ok=True)
+            if np.all([left > 0, right < len(signal)]):
+                one_dim_data_dir = osp.join(output_dir, '1D', name, sig_name, label)
+                two_dim_data_dir = osp.join(output_dir, '2D', name, sig_name, label)
+                os.makedirs(one_dim_data_dir, exist_ok=True)
+                os.makedirs(two_dim_data_dir, exist_ok=True)
 
-                    filename = osp.join(one_dim_data_dir, '{}.npy'.format(peak))
-                    np.save(filename, signal[left:right])
-                    filename = osp.join(two_dim_data_dir, '{}.png'.format(peak))
+                filename = osp.join(one_dim_data_dir, '{}.npy'.format(peak))
+                np.save(filename, signal[left:right])
+                filename = osp.join(two_dim_data_dir, '{}.png'.format(peak))
 
-                    plot(signal[left:right], filename)
+                plot(signal[left:right], filename)
 
 
