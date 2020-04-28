dce4bbd9139bd2f11fcea8b28817af8e52db4bd3
Alexander Lyashuk <alexander.lyashuk@xperience.ai>
2020-04-29 01:36:44 +0300

    Centered dataset

--- scripts/dataset-generation.py
+++ scripts/dataset-generation.py
@@ -10,8 +10,8 @@ from sklearn.preprocessing import scale
 from wfdb import rdrecord
 
 # Choose from peak to peak or centered
-mode = [20, 20]
-# mode = 128
+# mode = [20, 20]
+mode = 128
 
 image_size = 128
 output_dir = '../data'
