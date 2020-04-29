666b89ec6f56448f6fc531a895fcbdeb61eb26af
Alexander Lyashuk <alexander.lyashuk@xperience.ai>
2020-04-30 00:31:15 +0300

    EfficientNetB4

--- models/models_2d.py
+++ models/models_2d.py
@@ -81,6 +81,6 @@ def ShuffleNet(num_classes=8):
     return model
 
 def EfficientNet(num_classes=8):
-    model = efficientnet.from_name('efficientnet-b3')
+    model = efficientnet.from_name('efficientnet-b4')
     model._fc = nn.Linear(model._fc.in_features, num_classes)
     return model
