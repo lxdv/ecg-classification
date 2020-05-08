7059c14142c4a92782edfd2afd137b12c40c143f
Alexander Lyashuk <alexander.lyashuk@xperience.ai>
2020-05-08 03:33:05 +0300

    Pipeline

---
+++ pipeline.py
@@ -0,0 +1,20 @@
+import argparse
+import json
+
+from pipelines import pipelines
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    config = json.loads(open(args.config).read())
+    pipeline_type = getattr(pipelines, config['type'])
+
+    print("Trainer: ", config['type'], pipeline_type)
+    pipeline = pipeline_type(config)
+    pipeline.run_pipeline()
---
+++ pipelines/pipelines.py
@@ -0,0 +1,20 @@
+from dataloaders.dataset1d import EcgPipelineDataset1D
+from models import models1d
+from pipelines.base_pipeline import BasePipeline
+
+
+class Pipeline1D(BasePipeline):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def _init_net(self):
+        model = getattr(models1d, self.config['model'])(num_classes=self.config['num_classes'])
+        model = model.to(self.config['device'])
+        return model
+
+    def _init_dataloader(self):
+        inference_loader = EcgPipelineDataset1D(self.config['ecg_data']).get_dataloader(
+            batch_size=self.config['batch_size'], num_workers=self.config['num_workers'], shuffle=False
+        )
+
+        return inference_loader
